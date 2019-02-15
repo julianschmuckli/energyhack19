577a66fe401adbc03c4539054bd97f9b81ae1c29
Julian Schmuckli <julian@schmuckli.net>
2019-02-15 15:28:27 +0100

    Updated chart with random numbers for testing

--- assets/js/custom/reference_week.js
+++ assets/js/custom/reference_week.js
@@ -6,22 +6,55 @@ function initializeChart() {
     var ctx = document.getElementById("chart-reference-week").getContext('2d');
     ctx.height = 500;
     var chart = new Chart(ctx, {
-        // The type of chart we want to create
         type: 'line',
-
-        // The data for our dataset
+        labels: ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag", "Samstag", "Sonntag"],
         data: {
-            labels: ["January", "February", "March", "April", "May", "June", "July"],
-            datasets: [{
-                label: "My First dataset",
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data: [0, 10, 5, 2, 20, 30, 45],
-            }]
+            datasets: [
+                {
+                    label: "Optimalwert",
+                    backgroundColor: 'green',
+                    borderColor: 'green',
+                    fill: false,
+                    data: randomDataArray(7)
+                },
+                {
+                    label: "Soll",
+                    backgroundColor: 'red',
+                    borderColor: 'red',
+                    fill: false,
+                    data: randomDataArray(7)
+                },
+                {
+                    label: "Ist",
+                    backgroundColor: 'yellow',
+                    borderColor: 'yellow',
+                    fill: false,
+                    data: randomDataArray(7)
+                }
+            ],
         },
 
         // Configuration options go here
-        options: {}
+        options: {
+            scales: {
+                yAxes: [{
+                    type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+                    display: true,
+                    position: 'left',
+                    id: 'y-axis-1',
+                }]
+            }
+        }
     });
+}
 
+function randomDataArray(length) { //Random number creator
+    var data = [];
+
+    for (var i = 0; i < length; i++) {
+        var current_number = Math.floor((Math.random() * 100) + 1);
+        data.push(current_number);
+    }
+
+    return data;
 }
