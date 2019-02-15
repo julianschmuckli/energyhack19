b3e76fd6088ff8a1b8d2e663b5a42f61dab9b906
Julian Schmuckli <julian@schmuckli.net>
2019-02-15 15:07:27 +0100

    Fixed ChartJS

--- assets/js/custom/reference_week.js
+++ assets/js/custom/reference_week.js
@@ -1,3 +1,27 @@
+$(document).ready(function () {
+    initializeChart();
+});
+
 function initializeChart() {
+    var ctx = document.getElementById("chart-reference-week").getContext('2d');
+    ctx.height = 500;
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: ["January", "February", "March", "April", "May", "June", "July"],
+            datasets: [{
+                label: "My First dataset",
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: [0, 10, 5, 2, 20, 30, 45],
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
 
 }
