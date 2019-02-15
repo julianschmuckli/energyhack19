b2795682f798306dbf9dc972a9bc4cbbab9a1842
Nils <nils.egger@hotmail.ch>
2019-02-15 17:43:08 +0100

    parameter form

--- assets/js/custom/script.js
+++ assets/js/custom/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    $("#parameter").click(function() {
+    $("#open-parameters").click(function() {
         $("#parameters-content").slideToggle();
         $("#open-parameters").toggleClass("fa-rotate-180");
     });
